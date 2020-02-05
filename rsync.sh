17c08e287eeea969e9e70ee77fda5aaf8767d986
root <root@ip-172-31-47-246.ap-east-1.compute.internal>
2020-02-05 11:38:49 +0000

    new file:   rsync.sh 	modified:   yourdomain.com/0_docker_run_test.sh

---
+++ rsync.sh
@@ -0,0 +1,1 @@
+rsync -av /home/ /tmp/default_paths_for_docker/home/
---
+++ yourdomain.com/0_docker_run_test.sh
@@ -0,0 +1,1 @@
+docker run --privileged --name centos7-remi-php-test -v /sys/fs/cgroup:/sys/fs/cgroup:ro -d --restart always centos7-remi-php
