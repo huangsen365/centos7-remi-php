c64ab2ff970e546075d5a2be63e3d76870bd39c9
huangsen365 <huangsen365@gmail.com>
2020-02-06 16:23:37 +0800

    modified:   yourdomain.com/4_docker_run_centos7-remi-php-docker_yourdomain.com.sh 	modified:   yourdomain.com/5_docker_exec_mkdir_chown_chmod.sh

--- yourdomain.com/5_docker_exec_mkdir_chown_chmod.sh
+++ yourdomain.com/5_docker_exec_mkdir_chown_chmod.sh
@@ -1,5 +1,10 @@
 docker exec centos7-remi-php-docker_yourdomain.com sh /root/mkdir_chown_chmod.sh
 docker exec centos7-remi-php-docker_yourdomain.com cp -p /tmp/httpd_XX01_www.yourdomain.com.conf /etc/httpd/conf2.d/httpd_7201_www.yourdomain.com.conf
+docker exec centos7-remi-php-docker_yourdomain.com cp -p /etc/opt/remi/php70/php-fpm.d/php-fpm_7001_www.yourdomain.com_NEW2.conf_template /etc/opt/remi/php70/php-fpm2.d/php-fpm_7001_www.yourdomain.com_NEW2.conf
+docker exec centos7-remi-php-docker_yourdomain.com cp -p /etc/opt/remi/php71/php-fpm.d/php-fpm_7101_www.yourdomain.com_NEW2.conf_template /etc/opt/remi/php71/php-fpm2.d/php-fpm_7101_www.yourdomain.com_NEW2.conf
+docker exec centos7-remi-php-docker_yourdomain.com cp -p /etc/opt/remi/php72/php-fpm.d/php-fpm_7201_www.yourdomain.com_NEW2.conf_template /etc/opt/remi/php72/php-fpm2.d/php-fpm_7201_www.yourdomain.com_NEW2.conf
+docker exec centos7-remi-php-docker_yourdomain.com cp -p /etc/opt/remi/php73/php-fpm.d/php-fpm_7301_www.yourdomain.com_NEW2.conf_template /etc/opt/remi/php73/php-fpm2.d/php-fpm_7301_www.yourdomain.com_NEW2.conf
+docker exec centos7-remi-php-docker_yourdomain.com cp -p /etc/opt/remi/php74/php-fpm.d/php-fpm_7401_www.yourdomain.com_NEW2.conf_template /etc/opt/remi/php74/php-fpm2.d/php-fpm_7401_www.yourdomain.com_NEW2.conf
 mkdir -p ./volumes/volume_yourdomain.com/var/www/www.yourdomain.com/www
 cp ../info.php ./volumes/volume_yourdomain.com/var/www/www.yourdomain.com/www/info.php
 cp ../gitpull.php ./volumes/volume_yourdomain.com/var/www/www.yourdomain.com/www/gitpull.php
