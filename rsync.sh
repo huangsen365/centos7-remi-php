2e5744348d0a9a0ef54284554eaa4edcbcd20035
huangsen365 <huangsen365@gmail.com>
2020-02-06 18:31:31 +0800

    modified:   rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -3,17 +3,16 @@ rsync -av /root/ /tmp/default_paths_for_docker/root/
 rsync -av /var/www/ /tmp/default_paths_for_docker/var/www/
 rsync -av /var/log/ /tmp/default_paths_for_docker/var/log/
 rsync -av /etc/httpd/conf.d/ --exclude '*' /tmp/default_paths_for_docker/etc/httpd/conf2.d/
-# rsync -av /tmp/httpd_XX01_www.yourdomain.com.conf /tmp/default_paths_for_docker/etc/httpd/conf2.d/
 rsync -av /etc/opt/remi/php70/php-fpm.d/ --exclude '*' /tmp/default_paths_for_docker/etc/opt/remi/php70/php-fpm2.d/
 rsync -av /etc/opt/remi/php70/php-fpm.d/php-fpm_7001_www.yourdomain.com_NEW2.conf_template /tmp/default_paths_for_docker/etc/opt/remi/php70/php-fpm2.d/php-fpm_7001_www.yourdomain.com_NEW2.conf
 rsync -av /etc/opt/remi/php71/php-fpm.d/ --exclude '*' /tmp/default_paths_for_docker/etc/opt/remi/php71/php-fpm2.d/
-rsync -av /etc/opt/remi/php71/php-fpm.d/php-fpm_7001_www.yourdomain.com_NEW2.conf_template /tmp/default_paths_for_docker/etc/opt/remi/php71/php-fpm2.d/php-fpm_7001_www.yourdomain.com_NEW2.conf
+rsync -av /etc/opt/remi/php71/php-fpm.d/php-fpm_7101_www.yourdomain.com_NEW2.conf_template /tmp/default_paths_for_docker/etc/opt/remi/php71/php-fpm2.d/php-fpm_7101_www.yourdomain.com_NEW2.conf
 rsync -av /etc/opt/remi/php72/php-fpm.d/ --exclude '*' /tmp/default_paths_for_docker/etc/opt/remi/php72/php-fpm2.d/
-rsync -av /etc/opt/remi/php72/php-fpm.d/php-fpm_7001_www.yourdomain.com_NEW2.conf_template /tmp/default_paths_for_docker/etc/opt/remi/php72/php-fpm2.d/php-fpm_7001_www.yourdomain.com_NEW2.conf
+rsync -av /etc/opt/remi/php72/php-fpm.d/php-fpm_7201_www.yourdomain.com_NEW2.conf_template /tmp/default_paths_for_docker/etc/opt/remi/php72/php-fpm2.d/php-fpm_7201_www.yourdomain.com_NEW2.conf
 rsync -av /etc/opt/remi/php73/php-fpm.d/ --exclude '*' /tmp/default_paths_for_docker/etc/opt/remi/php73/php-fpm2.d/
-rsync -av /etc/opt/remi/php73/php-fpm.d/php-fpm_7001_www.yourdomain.com_NEW2.conf_template /tmp/default_paths_for_docker/etc/opt/remi/php73/php-fpm2.d/php-fpm_7001_www.yourdomain.com_NEW2.conf
+rsync -av /etc/opt/remi/php73/php-fpm.d/php-fpm_7301_www.yourdomain.com_NEW2.conf_template /tmp/default_paths_for_docker/etc/opt/remi/php73/php-fpm2.d/php-fpm_7301_www.yourdomain.com_NEW2.conf
 rsync -av /etc/opt/remi/php74/php-fpm.d/ --exclude '*' /tmp/default_paths_for_docker/etc/opt/remi/php74/php-fpm2.d/
-rsync -av /etc/opt/remi/php74/php-fpm.d/php-fpm_7001_www.yourdomain.com_NEW2.conf_template /tmp/default_paths_for_docker/etc/opt/remi/php74/php-fpm2.d/php-fpm_7001_www.yourdomain.com_NEW2.conf
+rsync -av /etc/opt/remi/php74/php-fpm.d/php-fpm_7401_www.yourdomain.com_NEW2.conf_template /tmp/default_paths_for_docker/etc/opt/remi/php74/php-fpm2.d/php-fpm_7401_www.yourdomain.com_NEW2.conf
 
 
 rsync -av /var/opt/remi/php70/lib/php/opcache/ /tmp/default_paths_for_docker/var/opt/remi/php70/lib/php/opcache/
