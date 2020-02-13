c811f0fa29e752504cdfbd5291861c6d1c11b3a1
huangsen365 <huangsen365@gmail.com>
2020-02-13 22:04:31 +0800

    modified:   tmp.sh
    	modified:   tmp.sh

--- yourdomain.com/tmp.sh
+++ yourdomain.com/tmp.sh
@@ -1,3 +1,17 @@
+docker ps -a | grep c_httpd_
+
+if [ $? -eq 0 ]
+then
+    echo "Found docker containers named as c_httpd_* , please manage them properly and then rerun this script!"
+    exit
+else
+    echo "Not found existing docker containers named as c_httpd_* , this script will continue run in 3 seconds..."
+    sleep 3
+    echo "ready go in 1s!"
+    sleep 1
+    echo "go now!"
+fi
+
 sh ./0_docker_run_test.sh
 sh ./1_docker_cp_default_paths_for_docker_to_volumes.sh
 sh ./2_cp_default_path_to_volume_yourdomain.com.sh
