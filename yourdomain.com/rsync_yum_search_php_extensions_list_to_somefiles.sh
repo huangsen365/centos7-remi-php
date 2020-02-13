ed939b689f027af28ec70fd000b22d47d04adf2d
huangsen365 <huangsen365@gmail.com>
2020-02-13 23:23:42 +0800

    modified:   .gitignore 	new file:   somefiles/yum_search_php_extensions_list_php70.txt 	new file:   somefiles/yum_search_php_extensions_list_php71.txt 	new file:   somefiles/yum_search_php_extensions_list_php72.txt 	new file:   somefiles/yum_search_php_extensions_list_php73.txt 	new file:   somefiles/yum_search_php_extensions_list_php74.txt 	new file:   yourdomain.com/rsync_yum_search_php_extensions_list_to_somefiles.sh

---
+++ yourdomain.com/rsync_yum_search_php_extensions_list_to_somefiles.sh
@@ -0,0 +1,6 @@
+rsync -av volumes/volume_yourdomain.com/root/yum_search_php_extensions_list_php70.txt ../somefiles/yum_search_php_extensions_list_php70.txt
+rsync -av volumes/volume_yourdomain.com/root/yum_search_php_extensions_list_php71.txt ../somefiles/yum_search_php_extensions_list_php71.txt
+rsync -av volumes/volume_yourdomain.com/root/yum_search_php_extensions_list_php72.txt ../somefiles/yum_search_php_extensions_list_php72.txt
+rsync -av volumes/volume_yourdomain.com/root/yum_search_php_extensions_list_php73.txt ../somefiles/yum_search_php_extensions_list_php73.txt
+rsync -av volumes/volume_yourdomain.com/root/yum_search_php_extensions_list_php74.txt ../somefiles/yum_search_php_extensions_list_php74.txt
+
