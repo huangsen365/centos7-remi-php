eb81096a1e37d0225ae56bd5fb9024c55a68526c
root <root@ip-172-31-47-246.ap-east-1.compute.internal>
2020-02-13 21:32:50 +0800

    modified:   6_docker_exec_it_bash.sh 	modified:   tmp.sh 	modified:   x_1_docker_stop_and_rm_centos7-remi-php-docker_yourdomain.com.sh

--- yourdomain.com/tmp.sh
+++ yourdomain.com/tmp.sh
@@ -3,5 +3,5 @@ sh ./1_docker_cp_default_paths_for_docker_to_volumes.sh
 sh ./2_cp_default_path_to_volume_yourdomain.com.sh
 sh ./3_docker_stop_and_rm_mycentos.sh
 sh ./4_docker_run_centos7-remi-php-docker_yourdomain.com.sh
-sh ./5_docker_exec_mkdir_chown_chmod.sh
+#sh ./5_docker_exec_mkdir_chown_chmod.sh
 sh ./6_docker_exec_it_bash.sh
