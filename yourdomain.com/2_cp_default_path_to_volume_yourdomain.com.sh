3a9755bdb5da4447fea913b725d50a7175f7c621
huangsen365 <huangsen365@gmail.com>
2020-02-15 22:13:40 +0800

    modified:   yourdomain.com/2_cp_default_path_to_volume_yourdomain.com.sh

--- yourdomain.com/2_cp_default_path_to_volume_yourdomain.com.sh
+++ yourdomain.com/2_cp_default_path_to_volume_yourdomain.com.sh
@@ -1,2 +1,3 @@
 #cp -pR volumes/default_paths_for_docker/ volumes/volume_yourdomain.com/
-cp -pR volumes/tmp/default_paths_for_docker/ volumes/volume_yourdomain.com/
+#cp -pR volumes/tmp/default_paths_for_docker/ volumes/volume_yourdomain.com/
+rsync -av volumes/tmp/default_paths_for_docker/ volumes/volume_yourdomain.com/
