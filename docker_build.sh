38986508a35b3bea588d198d1a96b0c87e69b3d1
huangsen365 <huangsen365@gmail.com>
2020-05-04 17:01:26 +0800

    modified:   docker_build.sh 	modified:   docker_build_no-cache.sh

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,5 @@
 date_time=$(date +%Y-%m-%d_%H%M%S)
-docker build -t centos7-remi-php . > ./docker_build_log/$date_time.txt &
+docker build -t huangsen365/centos7-remi-php . > ./docker_build_log/$date_time.txt &
 # 稍等一秒钟，等待以上$date_time.txt文件创建好
 sleep 1
 tailf ./docker_build_log/$date_time.txt
--- docker_build_no-cache.sh
+++ docker_build_no-cache.sh
@@ -1,5 +1,5 @@
 date_time=$(date +%Y-%m-%d_%H%M%S)
 #docker build -t centos7-remi-php . > ./docker_build_log/$(date +%Y-%m-%d_%H%M%S).txt &
-docker build --no-cache -t centos7-remi-php . > ./docker_build_log/$date_time.txt &
+docker build --no-cache -t huangsen365/centos7-remi-php . > ./docker_build_log/$date_time.txt &
 tailf ./docker_build_log/$date_time.txt
 
