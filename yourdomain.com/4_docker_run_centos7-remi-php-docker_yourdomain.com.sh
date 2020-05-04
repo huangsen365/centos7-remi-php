f3c9bca14e251fe2cef0931498a3fa1ba9ddb333
huangsen365 <huangsen365@gmail.com>
2020-05-04 22:43:13 +0800

    modified:   yourdomain.com/4_docker_run_centos7-remi-php-docker_yourdomain.com.sh

--- yourdomain.com/4_docker_run_centos7-remi-php-docker_yourdomain.com.sh
+++ yourdomain.com/4_docker_run_centos7-remi-php-docker_yourdomain.com.sh
@@ -13,6 +13,7 @@ cotainer_name=c_$date_and_hostname
 rsync -av --exclude=.ssh $current_dir/volumes/volume_yourdomain.com/home/ $current_dir/volumes/volume_yourdomain.com/$home_dir_name/
 rsync -av --exclude=.ssh $current_dir/volumes/volume_yourdomain.com/root/ $current_dir/volumes/volume_yourdomain.com/$root_dir_name/
 rsync -av $current_dir/volumes/volume_yourdomain.com/var/log/ $current_dir/volumes/volume_yourdomain.com/var/$log_dir_name/
+rsync -av $current_dir/volumes/volume_yourdomain.com/var/spool/cron/ $current_dir/volumes/volume_yourdomain.com/var/spool/$cron_dir_name/
 
 docker run -d \
   --privileged \
