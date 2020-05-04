aea41517c5398f668edf08b11492f3258042f102
huangsen365 <huangsen365@gmail.com>
2020-05-04 22:11:25 +0800

    modified:   1_docker_cp_default_paths_for_docker_to_volumes.sh

--- yourdomain.com/1_docker_cp_default_paths_for_docker_to_volumes.sh
+++ yourdomain.com/1_docker_cp_default_paths_for_docker_to_volumes.sh
@@ -1,3 +1,4 @@
 #docker cp -a centos7-remi-php-test:/tmp/default_paths_for_docker ./volumes/
 docker cp -a centos7-remi-php-test:/tmp/default_paths_for_docker.tar.gz ./volumes/
+rm -rf ./volumes/tmp/default_paths_for_docker
 tar zxvf ./volumes/default_paths_for_docker.tar.gz -C ./volumes/
