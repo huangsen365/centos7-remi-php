6eea1d2d1b149145f95ef69134638843932b3f79
huangsen365 <huangsen365@gmail.com>
2020-05-04 13:53:38 +0800

    Update docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,4 @@
 date_time=$(date +%Y-%m-%d_%H%M%S)
-#docker build -t centos7-remi-php . > ./docker_build_log/$(date +%Y-%m-%d_%H%M%S).txt &
 docker build -t centos7-remi-php . > ./docker_build_log/$date_time.txt &
 sleep 1
 tailf ./docker_build_log/$date_time.txt
