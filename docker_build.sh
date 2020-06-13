2992455bef5ed02edd67c1ef46cad777e24e0006
root <root@ip-172-31-28-95.ap-east-1.compute.internal>
2020-06-13 22:08:47 +0800

    modified:   docker_build.sh 	modified:   docker_build_no-cache.sh

--- docker_build.sh
+++ docker_build.sh
@@ -2,4 +2,4 @@ date_time=$(date +%Y-%m-%d_%H%M%S)
 docker build -t huangsen365/centos7-remi-php . > ./docker_build_log/$date_time.txt &
 # 稍等一秒钟，等待以上$date_time.txt文件创建好
 sleep 1
-tailf ./docker_build_log/$date_time.txt
+tail -f ./docker_build_log/$date_time.txt
--- docker_build_no-cache.sh
+++ docker_build_no-cache.sh
@@ -1,5 +1,4 @@
 date_time=$(date +%Y-%m-%d_%H%M%S)
 #docker build -t centos7-remi-php . > ./docker_build_log/$(date +%Y-%m-%d_%H%M%S).txt &
 docker build --no-cache -t huangsen365/centos7-remi-php . > ./docker_build_log/$date_time.txt &
-tailf ./docker_build_log/$date_time.txt
-
+tail -f ./docker_build_log/$date_time.txt
