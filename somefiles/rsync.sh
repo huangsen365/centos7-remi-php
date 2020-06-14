68a71cd850677ecefc3e600c1ef5b59c770fc2d7
huangsen365 <huangsen365@gmail.com>
2020-06-14 14:34:18 +0800

    Update rsync.sh

--- somefiles/rsync.sh
+++ somefiles/rsync.sh
@@ -1,3 +1,6 @@
+useradd sshuser
+usermod -aG apache sshuser
+
 rsync -av /home/ /tmp/default_paths_for_docker/home/
 rsync -av /root/ /tmp/default_paths_for_docker/root/
 rsync -av /var/www/ /tmp/default_paths_for_docker/var/www/
