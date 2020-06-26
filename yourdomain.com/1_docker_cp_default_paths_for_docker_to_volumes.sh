5e0d466e9295d59bb7ce56d91ce62da6889cfd53
root <root@ip-172-31-28-95.ap-east-1.compute.internal>
2020-06-26 18:31:16 +0800

    modified:   yourdomain.com/1_docker_cp_default_paths_for_docker_to_volumes.sh

--- yourdomain.com/1_docker_cp_default_paths_for_docker_to_volumes.sh
+++ yourdomain.com/1_docker_cp_default_paths_for_docker_to_volumes.sh
@@ -1,3 +1,3 @@
-docker cp -a centos7-remi-php-test:/tmp/default_paths_for_docker.tar.gz ./volumes/
-rm -rf ./volumes/tmp/default_paths_for_docker
+docker cp -a centos7-remi-php-test:/opt/centos7-remi-php/default_paths_for_docker.tar.gz ./volumes/
+rm -rf ./volumes/opt/centos7-remi-php/default_paths_for_docker
 tar zxvf ./volumes/default_paths_for_docker.tar.gz -C ./volumes/
