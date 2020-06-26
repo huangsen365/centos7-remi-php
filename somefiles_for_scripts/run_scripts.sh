97faabb321ba6a3e374770d5f7cdb87da97b6050
root <root@ip-172-31-28-95.ap-east-1.compute.internal>
2020-06-26 17:34:14 +0800

    modified:   somefiles_for_scripts/run_scripts.sh

--- somefiles_for_scripts/run_scripts.sh
+++ somefiles_for_scripts/run_scripts.sh
@@ -1,3 +1,3 @@
 RUN mv /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel.repo.backup \
     && mv /etc/yum.repos.d/epel-testing.repo /etc/yum.repos.d/epel-testing.repo.backup \
-    && wget -O /etc/yum.repos.d/epel.repo https://mirrors.aliyun.com/repo/epel-7.repo#
+    && wget -O /etc/yum.repos.d/epel.repo https://mirrors.aliyun.com/repo/epel-7.repo
