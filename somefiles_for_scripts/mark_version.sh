8de25273dd082264ad47971ab479fb2f29f08670
huangsen365 <huangsen365@gmail.com>
2020-07-15 20:52:40 +0800

    modified:   somefiles_for_scripts/mark_version.sh

--- somefiles_for_scripts/mark_version.sh
+++ somefiles_for_scripts/mark_version.sh
@@ -1,1 +1,3 @@
-git ls-remote https://github.com/huangsen365/centos7-remi-php.git | head -n 1 | cut -c1-8 > /opt/centos7-remi-php/version.txt
+# git ls-remote https://github.com/huangsen365/centos7-remi-php.git | head -n 1 | cut -c1-8 > /opt/centos7-remi-php/version.txt
+# date +%Y%m%d | awk -v version="$(git ls-remote https://github.com/huangsen365/centos7-remi-php.git | head -n 1 | cut -c1-8)" '{print "remi-php-"$0"-"version}' > /opt/centos7-remi-php/version.txt
+date +%Y%m%d-%H%M%S | awk -v version="$(git ls-remote https://github.com/huangsen365/centos7-remi-php.git | head -n 1 | cut -c1-8)" '{print "remi-php-"$0"-"version}' > /opt/centos7-remi-php/version.txt
