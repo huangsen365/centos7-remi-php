6f2b0e35740e8182a5632cfdab5f1e2561fea148
huangsen365 <huangsen365@gmail.com>
2020-07-15 23:10:44 +0800

    Update run_replace_repo_baseurl.sh

--- somefiles_for_scripts/run_replace_repo_baseurl.sh
+++ somefiles_for_scripts/run_replace_repo_baseurl.sh
@@ -10,5 +10,8 @@ sed -i 's/mirrorlist=http/#mirrorlist=http/g' /etc/yum.repos.d/CentOS-SCLo-scl-r
 sed -i 's/# baseurl=http:\/\/mirror.centos.org\//baseurl=https:\/\/mirrors.wansio.com\//g' /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo
 sed -i 's/#baseurl=http:\/\/mirror.centos.org\//baseurl=https:\/\/mirrors.wansio.com\//g' /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo
 
-sed -i 's/#baseurl=http\:\/\/download.fedoraproject.org/baseurl=http\:\/\/download-fedoraproject-org.wansio.com/g' /etc/yum.repos.d/epel.repo
-sed -i 's/metalink=https\:\/\/mirrors.fedoraproject.org/\#metalink=https\:\/\/mirrors-fedoraproject-org.wansio.com/g' /etc/yum.repos.d/epel.repo
+sed -i 's/#baseurl=http:\/\/download.fedoraproject.org/baseurl=http:\/\/download-fedoraproject-org.wansio.com/g' /etc/yum.repos.d/epel.repo
+sed -i 's/metalink=https:\/\/mirrors.fedoraproject.org/\#metalink=https:\/\/mirrors-fedoraproject-org.wansio.com/g' /etc/yum.repos.d/epel.repo
+
+sed -i 's/#baseurl=http:\/\/rpms.remirepo.net/baseurl=http:\/\/rpms-remirepo-net.wansio.com/g' /etc/yum.repos.d/remi-safe.repo
+sed -i 's/mirrorlist=http:\/\/cdn.remirepo.net/\#mirrorlist=http:\/\/cdn-remirepo-net.wansio.com/g' /etc/yum.repos.d/remi-safe.repo
