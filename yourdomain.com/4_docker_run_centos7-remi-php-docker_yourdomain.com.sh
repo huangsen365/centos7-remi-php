e807e02015b87689575da7c80c9fc550086be67d
huangsen365 <huangsen365@gmail.com>
2020-12-28 23:37:55 +0800

    Update 4_docker_run_centos7-remi-php-docker_yourdomain.com.sh

--- yourdomain.com/4_docker_run_centos7-remi-php-docker_yourdomain.com.sh
+++ yourdomain.com/4_docker_run_centos7-remi-php-docker_yourdomain.com.sh
@@ -1,3 +1,4 @@
+# next time maybe do checking if container is already running or not...
 cd /root/"$1"/centos7-remi-php-master/yourdomain.com
 current_dir=$(pwd)
 #httpd_log_dir_name=httpd_$(date +%Y-%m-%d_%H%M%S)_$(hostname)
