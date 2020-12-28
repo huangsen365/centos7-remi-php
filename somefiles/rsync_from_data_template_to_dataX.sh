9cc4d2303f531e219e3bb225c5af02d251b1c683
huangsen365 <huangsen365@gmail.com>
2020-12-28 22:54:39 +0800

    Update rsync_from_data_template_to_dataX.sh

--- somefiles/rsync_from_data_template_to_dataX.sh
+++ somefiles/rsync_from_data_template_to_dataX.sh
@@ -1,3 +1,4 @@
+# wget https://raw-githubusercontent-com-huangsen365.wansio.com/huangsen365/centos7-remi-php/master/somefiles/rsync_from_data_template_to_dataX.sh -O /root/rsync_from_data_template_to_dataX.sh
 if [ -n "$1" ]; then
     rsync -av /root/data_template/ /root/"$1"/
 else
