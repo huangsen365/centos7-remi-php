06caf7fc95e894de6415b54e800903788b6935f9
huangsen365 <huangsen365@gmail.com>
2020-12-28 23:06:47 +0800

    Update rsync_from_data_template_to_dataX.sh

--- somefiles/rsync_from_data_template_to_dataX.sh
+++ somefiles/rsync_from_data_template_to_dataX.sh
@@ -1,6 +1,6 @@
 # wget https://raw-githubusercontent-com-huangsen365.wansio.com/huangsen365/centos7-remi-php/master/somefiles/rsync_from_data_template_to_dataX.sh -O /root/rsync_from_data_template_to_dataX.sh
 if [ -n "$1" ]; then
-    rsync -av /root/data_template/ /root/"$1"/
+    rsync -av /root/data_template/* /root/"$1"/
 else
     echo "dataX argv is NOT set"
 fi
