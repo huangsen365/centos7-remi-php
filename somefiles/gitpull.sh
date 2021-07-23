2208333f39fb7ece0d9b705a4237e923772ef828
huangsen365 <huangsen365@gmail.com>
2021-07-23 14:52:36 +0800

    Update gitpull.sh

--- somefiles/gitpull.sh
+++ somefiles/gitpull.sh
@@ -1,4 +1,5 @@
 # Please add 'gitpull.sh' into the .gitignore file
+# https://github.com/huangsen365/centos7-remi-php/blob/master/somefiles/gitpull.sh
 # wget https://raw.githubusercontent.com/huangsen365/centos7-remi-php/master/somefiles/gitpull.sh -O gitpull.sh
 # wget https://raw-githubusercontent-com-huangsen365.wansio.com/huangsen365/centos7-remi-php/master/somefiles/gitpull.sh -O gitpull.sh
 git pull
