8a55bc8985d837db714810761c3794449bdd992d
huangsen365 <huangsen365@gmail.com>
2021-11-15 22:30:23 +0800

    Update 6_docker_exec_it_bash.sh

--- yourdomain.com/6_docker_exec_it_bash.sh
+++ yourdomain.com/6_docker_exec_it_bash.sh
@@ -1,3 +1,3 @@
 #docker exec -it centos7-remi-php-docker_yourdomain.com bash
 #docker exec -it $(docker ps -aq --filter name=c_* | tail -n 1) bash
-docker exec -it $(docker ps -aq --filter ancestor=huangsen365/centos7-remi-php | tail -n 1) bash
+docker exec -it $(docker ps -aq --filter name=c_* --filter ancestor=huangsen365/centos7-remi-php | tail -n 1) bash
