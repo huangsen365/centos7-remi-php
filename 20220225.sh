ed60f64d16af02213395c3f49d3b708cdf05292f
huangsen365 <huangsen365@gmail.com>
2022-02-25 22:29:50 +0800

    Create 20220225.sh

---
+++ 20220225.sh
@@ -0,0 +1,7 @@
+# vim mysqldump_20220225.sh
+mysqldump -uroot -p -hrm-wz9213r92fb012su4.mysql.rds.aliyuncs.com db_1jiaquan > db_1jiaquan.sql
+mysqldump -uroot -p -hrm-wz9213r92fb012su4.mysql.rds.aliyuncs.com db_1jiaquanyufabu > db_1jiaquanyufabu.sql
+mysqldump -uroot -p -hrm-wz9213r92fb012su4.mysql.rds.aliyuncs.com db_crm > db_crm.sql
+mysqldump -uroot -p -hrm-wz9213r92fb012su4.mysql.rds.aliyuncs.com db_exiaoshou > db_exiaoshou.sql
+mysqldump -uroot -p -hrm-wz9213r92fb012su4.mysql.rds.aliyuncs.com db_tiyan > db_tiyan.sql
+mysqldump -uroot -p -hrm-wz9213r92fb012su4.mysql.rds.aliyuncs.com db_yufabucrm > db_yufabucrm.sql
