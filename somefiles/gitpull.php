aa86da01c06d075b505f626c5725455d5f8b4a82
huangsen365 <huangsen365@gmail.com>
2022-11-13 17:15:19 +0800

    Update gitpull.php

--- somefiles/gitpull.php
+++ somefiles/gitpull.php
@@ -1,4 +1,5 @@
 <!--
+# 模板提供商（云计算服务器运维）: https://www.wansio.com
 本脚本免费开源，代码精简易用，若开发者在执行gitpull动作拉取更新代码过程中遇到 bug ，请尝试自行修复或者联系项目作者。项目链接为 https://github.com/huangsen365/centos7-remi-php/blob/master/somefiles/gitpull.php
 -->
 
@@ -33,5 +34,6 @@ echo "<a target='_blank' href='" . $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER
 </p>
 
 <!--
+# 模板提供商（云计算服务器运维）: https://www.wansio.com
 本脚本免费开源，代码精简易用，若开发者在执行gitpull动作拉取更新代码过程中遇到 bug ，请尝试自行修复或者联系项目作者。项目链接为 https://github.com/huangsen365/centos7-remi-php/blob/master/somefiles/gitpull.php
 -->
