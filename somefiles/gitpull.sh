f804ee10f88360e34acead94d8cbe315cbace0ba
huangsen365 <huangsen365@gmail.com>
2022-11-13 17:15:39 +0800

    Update gitpull.sh

--- somefiles/gitpull.sh
+++ somefiles/gitpull.sh
@@ -1,3 +1,4 @@
+# 模板提供商（云计算服务器运维）: https://www.wansio.com
 # Please add 'gitpull.sh' into the .gitignore file
 # https://github.com/huangsen365/centos7-remi-php/blob/master/somefiles/gitpull.sh
 # wget https://raw.githubusercontent.com/huangsen365/centos7-remi-php/master/somefiles/gitpull.sh -O gitpull.sh
